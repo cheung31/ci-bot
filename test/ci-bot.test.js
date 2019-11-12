5cc60ddf47a2d1c702270e034a80afaf2882c11b
Ryan Cheung <cheung31@gmail.com>
2019-11-11 16:27:16 -0800

    Fix test

--- test/ci-bot.test.js
+++ test/ci-bot.test.js
@@ -23,7 +23,7 @@ describe('ci-bot', () => {
     }
 
     probot = new Probot({
-      id: 123,
+      id: 123
     })
     // probot.app.auth = jest.fn(() => Promise.resolve(mockGitHubApi)); // https://github.com/probot/probot/blob/master/src/application.ts#L485-L527
     // Load our app into probot
@@ -40,6 +40,7 @@ describe('ci-bot', () => {
       .get('/api/v1.1/project/github/Codertocat/Hello-World/999')
       .reply(200, buildResponse)
     let outputResponse = fs.readFileSync(path.join(__dirname, 'fixtures', 'circleci.action-output.json'))
+
     nock('https://circle-production-action-output.s3.amazonaws.com:443')
       .get('/b39162dfb75778a9c74f9cd5-5dc7484e5bfb091ae4c9175d-104-0?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20191112T001622Z&X-Amz-SignedHeaders=host&X-Amz-Expires=431999&X-Amz-Credential=AKIAIJNI6FA5RIAFFQ7Q%2F20191112%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=6e09ebe1642e7f2ef035603b6cb917a0888409cb824d65dacfbf299097497759')
       .reply(200, outputResponse)
